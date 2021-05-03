d8b2d948a2a45b0eba92c076175d5ff325de3034
byoungyong choi <jojosale99@naver.com>
2021-05-04 00:09:22 +0900

    feat (compile) : [TDD] 랜던값에 따른 자동차 이동

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -2,12 +2,26 @@ package racingcar.domain.car;
 
 public class Car {
     private final Name name;
+    private Position position;
+    private static final int MOVE_CONDITION = 4;
+
 
     public Car(final String name){
         this.name = new Name(name);
+        this.position = new Position(0);
     }
 
     public Name getName() {
         return name;
+    }
+
+    public void tryMove(int moveNum){
+        if (moveNum >= MOVE_CONDITION){
+            position = position.run();
+        }
+    }
+
+    public int getPosition() {
+        return position.get();
     }
 }
--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -1,11 +1,13 @@
 package racingcar.domain.car;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import racingcar.domain.RandomUtil;
 
 public class CarTest {
 
@@ -23,4 +25,11 @@ public class CarTest {
         assertThatThrownBy(() -> new Car(name))
             .isInstanceOf(IllegalStateException.class);
     }
+
+    @Test
+    public void 차_이동(){
+        Car car = new Car("pobi");
+        car.tryMove(5);
+        assertThat(car.getPosition()).isEqualTo(1);
+    } 
 }
